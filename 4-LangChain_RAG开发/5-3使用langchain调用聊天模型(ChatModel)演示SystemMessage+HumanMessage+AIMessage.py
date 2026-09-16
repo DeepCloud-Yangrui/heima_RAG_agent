30d23bf5f8d29ea6aa250effd9fa2f02688baef3
yangrui <3226265880@qq.com>
2026-09-16 15:49:45 +0800

    5-使用langchain调用聊天模型(ChatModel)

---
+++ 4-LangChain_RAG开发/5-3使用langchain调用聊天模型(ChatModel)演示SystemMessage+HumanMessage+AIMessage.py
@@ -0,0 +1,29 @@
+"""
+案例：
+    演示SystemMessage+HumanMessage+AIMessage
+"""
+import os
+# 1. 导入现代化 Chat 客户端和消息类
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage,SystemMessage,AIMessage
+
+# 2. 初始化 Chat 客户端
+chat = ChatOpenAI(
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    model="qwen3.8-max-0902"
+)
+
+# 3. 构造提问消息
+# 这里的message是一个列表list
+message = [
+    SystemMessage(content="你是一名田园诗人"),
+    HumanMessage(content="给我写一首唐诗"),
+    AIMessage(content="梅子金黄杏子肥，麦花雪白菜花稀。日长篱落无人过，惟有蜻蜓蛱蝶飞。"),
+    HumanMessage(content="仿照上首，再写一首")
+]
+
+# 4. 列表包裹传入并流式输出
+for chunk in chat.stream(message):
+    print(chunk.content, end="", flush=True)
+
+print()
